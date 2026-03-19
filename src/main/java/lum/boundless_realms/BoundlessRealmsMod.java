45c6cb59a1fad3ee6c8f0a6ad2c722fd9068fae0
LumIsHere <lyuanze2011@gmail.com>
2026-03-20 02:24:00 +0800

    added initializations

--- src/main/java/lum/boundless_realms/BoundlessRealmsMod.java
+++ src/main/java/lum/boundless_realms/BoundlessRealmsMod.java
@@ -1,9 +1,12 @@
 package lum.boundless_realms;
 
+import lum.boundless_realms.block.ModBlockEntities;
+import lum.boundless_realms.block.ModBlocks;
 import lum.boundless_realms.command.InspectorAnswerCommand;
 import lum.boundless_realms.entity.ModEntities;
 import lum.boundless_realms.item.ModItemGroups;
 import lum.boundless_realms.item.ModItems;
+import lum.boundless_realms.recipe.ModRecipeTypes;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import org.slf4j.Logger;
@@ -18,6 +21,9 @@ public class BoundlessRealmsMod implements ModInitializer {
 		ModEntities.registerModEntities();
 		ModItems.registerModItems();
 		ModItemGroups.registerItemGroups();
+        ModBlocks.registerModBlocks();
+        ModBlockEntities.registerBlockEntities();
+        ModRecipeTypes.registerRecipes();
 
 		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->
 				InspectorAnswerCommand.register(dispatcher)
