b7fadce1cab091c6fffb90990f102b3e4a07b2da
LumIsHere <lyuanze2011@gmail.com>
2026-03-20 02:23:16 +0800

    changed how items are registered

--- src/main/java/lum/boundless_realms/item/ModItems.java
+++ src/main/java/lum/boundless_realms/item/ModItems.java
@@ -21,47 +21,43 @@ import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.function.Function;
 
 public class ModItems {
-    public static final Item AN_ITEM = registerItem("an_item", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BoundlessRealmsMod.MOD_ID,"an_item")))));
 
-    public static final Item MONEY = registerItem("money",
-            new Item(new Item.Settings().maxCount(99).registryKey(
-                    RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BoundlessRealmsMod.MOD_ID, "money"))
-            )));
+    public static final Item AN_ITEM = registerItem("an_item", Item::new, new Item.Settings());
 
-    public static final Item LUNCH_TICKET = registerItem("lunch_ticket",
-            new LunchTicketItem(new Item.Settings().maxCount(1).registryKey(
-                    RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BoundlessRealmsMod.MOD_ID, "lunch_ticket"))
-            )));
+    public static final Item MONEY = registerItem("money", Item::new, new Item.Settings().maxCount(99));
 
-    public static final Item BACKSTAB_TOTEM = registerItem("backstab_totem", new BackstabTotemItem(new Item.Settings().maxCount(1).registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BoundlessRealmsMod.MOD_ID, "backstab_totem")))));
-    public static final Item WITHER_FURY = registerItem("wither_fury",
-            new WitherFuryItem(new Item.Settings()
+    public static final Item LUNCH_TICKET = registerItem("lunch_ticket", LunchTicketItem::new,
+            new Item.Settings().maxCount(1));
+
+    public static final Item BACKSTAB_TOTEM = registerItem("backstab_totem", BackstabTotemItem::new,
+            new Item.Settings().maxCount(1));
+
+    public static final Item WITHER_FURY = registerItem("wither_fury", WitherFuryItem::new,
+            new Item.Settings()
                     .maxCount(1)
                     .attributeModifiers(createSwordAttributes(8.0f, -2.4f))
-                    .component(DataComponentTypes.TOOL, createSwordToolComponent())
-                    .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BoundlessRealmsMod.MOD_ID, "wither_fury")))
-            )
-    );
-    public static final Item TAIGA_ONE_SURVIVAL_MACHETE = registerItem("taiga_1_survival_machete",
-            new Item(new Item.Settings().sword(ToolMaterial.NETHERITE, 6.0F, -3.0F).registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BoundlessRealmsMod.MOD_ID,"taiga_1_survival_machete")))));
+                    .component(DataComponentTypes.TOOL, createSwordToolComponent()));
 
-    public static final Item ANGLERFISH_MASK = registerItem("anglerfish_mask",
-            new AnglerfishMaskItem(new Item.Settings()
-                    .maxCount(1).component(DataComponentTypes.EQUIPPABLE, EquippableComponent.builder(EquipmentSlot.HEAD).equipSound(SoundEvents.ITEM_ARMOR_EQUIP_LEATHER).build())
-                    .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BoundlessRealmsMod.MOD_ID, "anglerfish_mask")))
-            ));
+    public static final Item TAIGA_ONE_SURVIVAL_MACHETE = registerItem("taiga_1_survival_machete", Item::new,
+            new Item.Settings().sword(ToolMaterial.NETHERITE, 6.0F, -3.0F));
 
-    public static final Item PORTABLE_CRAFTING_TABLE = registerItem("portable_crafting_table",
-            new PortableCraftingTableItem(new Item.Settings().maxCount(1).registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BoundlessRealmsMod.MOD_ID, "portable_crafting_table")))));
+    public static final Item ANGLERFISH_MASK = registerItem("anglerfish_mask", AnglerfishMaskItem::new,
+            new Item.Settings()
+                    .maxCount(1)
+                    .component(DataComponentTypes.EQUIPPABLE, EquippableComponent.builder(EquipmentSlot.HEAD)
+                            .equipSound(SoundEvents.ITEM_ARMOR_EQUIP_LEATHER).build()));
 
-    public static final Item WALLET = registerItem("wallet",
-            new WalletItem(new Item.Settings().maxCount(1).registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BoundlessRealmsMod.MOD_ID,"wallet")))));
+    public static final Item PORTABLE_CRAFTING_TABLE = registerItem("portable_crafting_table", PortableCraftingTableItem::new,
+            new Item.Settings().maxCount(1));
 
-    public static final Item NETHER_FURNACE_UPGRADE_TEMPLATE = registerItem("nether_furnace_upgrade_template", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BoundlessRealmsMod.MOD_ID,"nether_furnace_upgrade_template")))));
+    public static final Item WALLET = registerItem("wallet", WalletItem::new,
+            new Item.Settings().maxCount(1));
 
+    public static final Item NETHER_FURNACE_UPGRADE_TEMPLATE = registerItem("nether_furnace_upgrade_template", Item::new,
+            new Item.Settings());
 
     private static AttributeModifiersComponent createSwordAttributes(double damage, double speed) {
         return AttributeModifiersComponent.builder()
@@ -88,8 +84,10 @@ public class ModItems {
         );
     }
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(BoundlessRealmsMod.MOD_ID, name), item);
+    private static <T extends Item> T registerItem(String name, Function<Item.Settings, T> factory, Item.Settings settings) {
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BoundlessRealmsMod.MOD_ID, name));
+        T item = factory.apply(settings.registryKey(key));
+        return Registry.register(Registries.ITEM, key, item);
     }
 
     public static void registerModItems() {
