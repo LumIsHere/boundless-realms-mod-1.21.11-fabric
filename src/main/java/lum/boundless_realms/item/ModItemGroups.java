d7b80ccde448ffeb226d1c9b587484aed65b41f9
LumIsHere <lyuanze2011@gmail.com>
2026-03-21 03:37:56 +0800

    new medical supply item group

--- src/main/java/lum/boundless_realms/item/ModItemGroups.java
+++ src/main/java/lum/boundless_realms/item/ModItemGroups.java
@@ -31,6 +31,15 @@ public class ModItemGroups {
                     })
                     .build());
 
+    public static final ItemGroup BOUNDLESS_REALMS_MEDICAL_ITEMS_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(BoundlessRealmsMod.MOD_ID, "boundless_realms_medical_items"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.AFAK_MED_KIT))
+                    .displayName(Text.translatable("itemgroup.boundless_realms.boundless_realms_medical_items"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ModItems.AFAK_MED_KIT);
+                    })
+                    .build());
+
     public static void registerItemGroups() {
         BoundlessRealmsMod.LOGGER.info("Registering item groups for " + BoundlessRealmsMod.MOD_ID);
     }
