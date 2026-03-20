cbe996aa3819cbf60695ad3ae80afc1626a04704
LumIsHere <lyuanze2011@gmail.com>
2026-03-20 22:04:22 +0800

    Changed

--- src/main/java/lum/boundless_realms/entity/ModEntities.java
+++ src/main/java/lum/boundless_realms/entity/ModEntities.java
@@ -4,54 +4,44 @@ import lum.boundless_realms.BoundlessRealmsMod;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ModEntities {
-
-    public static final Identifier LUNCH_TICKET_PROJECTILE_ID = entityId("lunch_ticket_projectile");
-
-    public static final RegistryKey<EntityType<LunchTicketProjectileEntity>> LUNCH_TICKET_PROJECTILE_KEY = entityKey(LUNCH_TICKET_PROJECTILE_ID);
+    private static final Map<EntityType<? extends LivingEntity>, DefaultAttributeContainer.Builder> ATTRIBUTE_REGISTRY = new HashMap<>();
 
     public static final EntityType<LunchTicketProjectileEntity> LUNCH_TICKET_PROJECTILE = registerEntity(
-            LUNCH_TICKET_PROJECTILE_ID,
-            LUNCH_TICKET_PROJECTILE_KEY,
-            EntityType.Builder.<LunchTicketProjectileEntity>create(
-                            LunchTicketProjectileEntity::new,
-                            SpawnGroup.MISC
-                    )
+            entityId("lunch_ticket_projectile"),
+            entityKey(entityId("lunch_ticket_projectile")),
+            EntityType.Builder.<LunchTicketProjectileEntity>create(LunchTicketProjectileEntity::new, SpawnGroup.MISC)
                     .dimensions(0.25F, 0.25F)
-                    .maxTrackingRange(4)
-                    .trackingTickInterval(10)
     );
 
-    public static final Identifier TICKET_INSPECTOR_ID = entityId("ticket_inspector");
-
-    public static final RegistryKey<EntityType<TicketInspectorEntity>> TICKET_INSPECTOR_KEY = entityKey(TICKET_INSPECTOR_ID);
-
-    public static final EntityType<TicketInspectorEntity> TICKET_INSPECTOR = registerEntity(
-            TICKET_INSPECTOR_ID,
-            TICKET_INSPECTOR_KEY,
-            EntityType.Builder.<TicketInspectorEntity>create(
-                            TicketInspectorEntity::new,
-                            SpawnGroup.CREATURE
-                    )
-                    .dimensions(0.6F, 1.95F)
-                    .maxTrackingRange(8)
-                    .trackingTickInterval(2)
+    public static final EntityType<TicketInspectorEntity> TICKET_INSPECTOR = registerLivingEntity(
+            "ticket_inspector",
+            EntityType.Builder.<TicketInspectorEntity>create(TicketInspectorEntity::new, SpawnGroup.CREATURE)
+                    .dimensions(0.6F, 1.95F),
+            TicketInspectorEntity.createAttributes()
     );
 
     public static void registerModEntities() {
-        BoundlessRealmsMod.LOGGER.info("Registering entities for " + BoundlessRealmsMod.MOD_ID);
+        ATTRIBUTE_REGISTRY.forEach(FabricDefaultAttributeRegistry::register);
+    }
 
-        FabricDefaultAttributeRegistry.register(
-                TICKET_INSPECTOR,
-                TicketInspectorEntity.createAttributes()
-        );
+    private static <T extends LivingEntity> EntityType<T> registerLivingEntity(String name, EntityType.Builder<T> builder, DefaultAttributeContainer.Builder attributes) {
+        Identifier id = entityId(name);
+        EntityType<T> type = registerEntity(id, entityKey(id), builder);
+        ATTRIBUTE_REGISTRY.put(type, attributes);
+        return type;
     }
 
     private static Identifier entityId(String name) {
@@ -63,11 +53,7 @@ public class ModEntities {
         return (RegistryKey<EntityType<T>>) (RegistryKey<?>) RegistryKey.of(RegistryKeys.ENTITY_TYPE, id);
     }
 
-    private static <T extends Entity> EntityType<T> registerEntity(
-            Identifier id,
-            RegistryKey<EntityType<T>> key,
-            EntityType.Builder<T> builder
-    ) {
+    private static <T extends Entity> EntityType<T> registerEntity(Identifier id, RegistryKey<EntityType<T>> key, EntityType.Builder<T> builder) {
         return Registry.register(Registries.ENTITY_TYPE, id, builder.build(asWildcardKey(key)));
     }
 
