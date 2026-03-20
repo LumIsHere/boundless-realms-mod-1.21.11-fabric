4f2a0fd66c549faa8c2affa0376873edae47c182
Holly Yi <hollyyiishere@gmail.com>
2026-03-20 21:38:31 +0800

    added new 注释

--- src/main/java/lum/boundless_realms/entity/ModEntities.java
+++ src/main/java/lum/boundless_realms/entity/ModEntities.java
@@ -2,6 +2,7 @@ package lum.boundless_realms.entity;
 
 import lum.boundless_realms.BoundlessRealmsMod;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.registry.Registries;
@@ -12,45 +13,37 @@ import net.minecraft.util.Identifier;
 
 public class ModEntities {
 
-    public static final Identifier LUNCH_TICKET_PROJECTILE_ID =
-            Identifier.of(BoundlessRealmsMod.MOD_ID, "lunch_ticket_projectile");
+    public static final Identifier LUNCH_TICKET_PROJECTILE_ID = entityId("lunch_ticket_projectile");
 
-    public static final RegistryKey<EntityType<?>> LUNCH_TICKET_PROJECTILE_KEY =
-            RegistryKey.of(RegistryKeys.ENTITY_TYPE, LUNCH_TICKET_PROJECTILE_ID);
+    public static final RegistryKey<EntityType<LunchTicketProjectileEntity>> LUNCH_TICKET_PROJECTILE_KEY = entityKey(LUNCH_TICKET_PROJECTILE_ID);
 
-    public static final EntityType<LunchTicketProjectileEntity> LUNCH_TICKET_PROJECTILE =
-            Registry.register(
-                    Registries.ENTITY_TYPE,
-                    LUNCH_TICKET_PROJECTILE_ID,
-                    EntityType.Builder.<LunchTicketProjectileEntity>create(
-                                    LunchTicketProjectileEntity::new,
-                                    SpawnGroup.MISC
-                            )
-                            .dimensions(0.25F, 0.25F)
-                            .maxTrackingRange(4)
-                            .trackingTickInterval(10)
-                            .build(LUNCH_TICKET_PROJECTILE_KEY)
-            );
+    public static final EntityType<LunchTicketProjectileEntity> LUNCH_TICKET_PROJECTILE = registerEntity(
+            LUNCH_TICKET_PROJECTILE_ID,
+            LUNCH_TICKET_PROJECTILE_KEY,
+            EntityType.Builder.<LunchTicketProjectileEntity>create(
+                            LunchTicketProjectileEntity::new,
+                            SpawnGroup.MISC
+                    )
+                    .dimensions(0.25F, 0.25F)
+                    .maxTrackingRange(4)
+                    .trackingTickInterval(10)
+    );
 
-    public static final Identifier TICKET_INSPECTOR_ID =
-            Identifier.of(BoundlessRealmsMod.MOD_ID, "ticket_inspector");
+    public static final Identifier TICKET_INSPECTOR_ID = entityId("ticket_inspector");
 
-    public static final RegistryKey<EntityType<?>> TICKET_INSPECTOR_KEY =
-            RegistryKey.of(RegistryKeys.ENTITY_TYPE, TICKET_INSPECTOR_ID);
+    public static final RegistryKey<EntityType<TicketInspectorEntity>> TICKET_INSPECTOR_KEY = entityKey(TICKET_INSPECTOR_ID);
 
-    public static final EntityType<TicketInspectorEntity> TICKET_INSPECTOR =
-            Registry.register(
-                    Registries.ENTITY_TYPE,
-                    TICKET_INSPECTOR_ID,
-                    EntityType.Builder.<TicketInspectorEntity>create(
-                                    TicketInspectorEntity::new,
-                                    SpawnGroup.CREATURE
-                            )
-                            .dimensions(0.6F, 1.95F)
-                            .maxTrackingRange(8)
-                            .trackingTickInterval(2)
-                            .build(TICKET_INSPECTOR_KEY)
-            );
+    public static final EntityType<TicketInspectorEntity> TICKET_INSPECTOR = registerEntity(
+            TICKET_INSPECTOR_ID,
+            TICKET_INSPECTOR_KEY,
+            EntityType.Builder.<TicketInspectorEntity>create(
+                            TicketInspectorEntity::new,
+                            SpawnGroup.CREATURE
+                    )
+                    .dimensions(0.6F, 1.95F)
+                    .maxTrackingRange(8)
+                    .trackingTickInterval(2)
+    );
 
     public static void registerModEntities() {
         BoundlessRealmsMod.LOGGER.info("Registering entities for " + BoundlessRealmsMod.MOD_ID);
@@ -60,5 +53,26 @@ public class ModEntities {
                 TicketInspectorEntity.createAttributes()
         );
     }
-}
 
+    private static Identifier entityId(String name) {
+        return Identifier.of(BoundlessRealmsMod.MOD_ID, name);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T extends Entity> RegistryKey<EntityType<T>> entityKey(Identifier id) {
+        return (RegistryKey<EntityType<T>>) (RegistryKey<?>) RegistryKey.of(RegistryKeys.ENTITY_TYPE, id);
+    }
+
+    private static <T extends Entity> EntityType<T> registerEntity(
+            Identifier id,
+            RegistryKey<EntityType<T>> key,
+            EntityType.Builder<T> builder
+    ) {
+        return Registry.register(Registries.ENTITY_TYPE, id, builder.build(asWildcardKey(key)));
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T extends Entity> RegistryKey<EntityType<?>> asWildcardKey(RegistryKey<EntityType<T>> key) {
+        return (RegistryKey<EntityType<?>>) (RegistryKey<?>) key;
+    }
+}
